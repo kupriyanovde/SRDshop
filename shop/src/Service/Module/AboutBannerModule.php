671b28ab46bce86f1d5710632aeac2b84ab90b21
kupriyanovde <kupriyanovde@ya.ru>
2025-07-19 11:28:39 +0600

    пример баннера почему выбирают нас

---
+++ shop/src/Service/Module/AboutBannerModule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service\Module;
+
+use App\Service\Contract\ModuleContentInterface;
+use Twig\Environment;
+
+class AboutBannerModule implements ModuleContentInterface
+{
+    public function __construct(
+        private Environment $twig
+    ) {}
+
+    public function getName(): string
+    {
+        return 'about_banner';
+    }
+
+    public function getContent(string $route, string $position): ?string
+    {
+        // Передаем данные в twig-шаблон
+        return $this->twig->render('module/about_banner.html.twig', [
+            // можно передать дополнительные данные, если нужно
+        ]);
+    }
+
+
+}
